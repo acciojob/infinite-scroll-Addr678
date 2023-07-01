b47c93507ad7ad848a7371825ff4614d51a84fc1
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-07-01 08:49:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
-//your code here!
+document.addEventListener("DOMContentLoaded", function () {
+  const infiList = document.getElementById("infi-list");
+
+  // Function to add a new list item
+  function addListItem() {
+    const listItem = document.createElement("li");
+    listItem.textContent = "List item";
+    infiList.appendChild(listItem);
+  }
+
+  // Add about 10 list items to the list by default
+  for (let i = 0; i < 10; i++) {
+    addListItem();
+  }
+
+  // Function to handle scrolling and add more list items at the end
+  function handleScroll() {
+    const { scrollTop, scrollHeight, clientHeight } = infiList;
+
+    if (scrollTop + clientHeight >= scrollHeight) {
+      // User has reached the end of the list, add 2 more list items
+      for (let i = 0; i < 2; i++) {
+        addListItem();
+      }
+    }
+  }
+
+  // Add event listener to the list to trigger infinite scrolling
+  infiList.addEventListener("scroll", handleScroll);
+});
+
+
 
